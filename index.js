3b2295e5c9cfb6a02371876482ade7343c1abbee
Eduardo <eduardolunardi942@gmail.com>
2020-03-19 23:06:13 -0300

    novo, atualizar, editar

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
 const cors = require('cors');
 const path = require('path');
 const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-    host      : 'localhost',
-    port      : '3306',
-    user      : 'root',
-    password  : '',
-    database  : 'loja',
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'loja',
 });
 
-connection.connect(function(err) {
-    if(err){
+connection.connect(function (err) {
+    if (err) {
         console.error('erro conectando banco: ' + err.stack);
         return;
     }
@@ -24,32 +24,66 @@ connection.connect(function(err) {
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use("/", function(req, res) {
 //     res.sendFile(path.join(__dirname + '/public/'))
 // });
 
-app.get('/listar', function(req, res) {
+app.get('/listar', function (req, res) {
     connection.query('select * from produtos',
-    function(error, results, fields) {
-        if(error)
-        res.json(error);
-        else
-            res.json(results);
-            connection.end();
-            console.log('executou a listagem!');   
-    }); 
+        function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else
+                res.json(results);
+            console.log('executou a listagem!');
+        });
 });
- 
+
+app.post('/enviar', function (req, res) {
+    console.log(req.body)
+    connection.query(`insert into produtos(nome, valor) values( '${req.body.nome} ',' ${req.body.valor} ')`,
+        function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else
+                res.json({ "nome": "1" });
+            console.log('executou o envio!');
+        });
+});
+
+app.delete('/deletar', function (req, res) {
+    console.log(req.body)
+    connection.query(`delete from produtos where id = ${req.body.id}`,
+        function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else
+                res.json({ "ID": "1" });
+            console.log('deletou!');
+        });
+});
+
+app.put('/up', function (req, res) {
+    console.log(req.body)
+    connection.query(`update produtos set nome='${req.body.nome}', valor='${req.body.valor}' where id = ${req.body.id})`,
+        function (error, results, fields) {
+            if (error)
+                res.json(error);
+            else
+                res.json({ "nome": "1" });
+            console.log('Atualizado!');
+        });
+});
 
 
 app.use(express.static(__dirname + '/public'));
-const PORT=80;
+const PORT = 80;
 app.listen(PORT);
 console.log('server on ' + PORT);
 
 // app.listen(PORT, function (res) {
 //     console.log('Banco conectado na porta 80:');
-    
+
 // });
